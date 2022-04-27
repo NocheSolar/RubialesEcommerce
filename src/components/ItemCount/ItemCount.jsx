3fa96edd83c7e2fe1617ebdbd8e1688049a0cc7f
NocheSolar <luciarubiales95@gmail.com>
2022-04-26 21:08:01 -0300

    agregue el itemcount

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './ItemCount.css';
+
+function ItemCount({ stock, initial, onAdd }) {
+    const [count, setCount] = useState(initial);
+    function increaseButton() {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+    function decreaseButton() {
+        if (count > initial) {
+            setCount(count - 1)
+        }
+    }
+    function onAdd (){
+        alert(`Tus productos se agregaron correctamente (${count}) `);
+        console.log (count);
+    }
+    return (
+        <div className='itemcountcontainer'>
+            <div>
+                <button onClick={() => decreaseButton()}>-</button>
+                <input readOnly value={count} />
+                <button onClick={() => increaseButton()}>+</button>
+            </div>
+            <button onClick={() => (count <= stock) && onAdd()}>Agregar al carrito</button>
+        </div>
+    )
+  
+}
+export default ItemCount
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount';
 import './itemlistcontainer.css';
 
 const container = ({greetings}) => {
   return (
-    <div className='containerList'>{greetings}</div>
+    <>
+      <div className='containerList'>{greetings}</div>
+      <ItemCount stock= {20} initial= {0}/>
+    </>
   )
 }
 
