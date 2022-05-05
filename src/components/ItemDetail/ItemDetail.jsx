4dee211d26ddbfe17f506640b577da56ef837da7
NocheSolar <luciarubiales95@gmail.com>
2022-05-05 19:05:36 -0300

    product

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { isCompositeComponent } from 'react-dom/test-utils';
 import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
 
 function ItemDetail ( {productsList}){
     return (
         <div>
-            
+            <img src="{productsList.image}" alt="" />
+            <h2> {productsList.tittle}</h2>
+            <h4>{productsList.price}</h4>
         </div>
+        //agregar contador
     )
 }
 
