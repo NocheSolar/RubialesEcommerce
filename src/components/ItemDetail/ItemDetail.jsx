0aa2a031dc3c6c735c87febfcfd4b41b9f6bdd0a
NocheSolar <luciarubiales95@gmail.com>
2022-05-12 21:50:43 -0300

    Cart

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,29 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
 import ItemCount from '../ItemCount/ItemCount';
-import Item from '../Item/Item';
 import Cart from '../Cart/Cart';
 
-function ItemDetail ({item}){
-    const [productsAvailable, setproductsAvailable] = useState (null);
-    function onAddCart (quantitytoAdd){
-        setproductsAvailable (quantitytoAdd);
-    }
+const ItemDetail = ({ product }) => {
+    const { tittle, price, image, stock, id } = product;
+    const [countToAdd, setCountToAdd] = useState(0);
+    const { addToCart, unitsPerProduct } = useCartContext();
 
+    const handleOnAdd = (count) => {
+        if (count + unitsPerProduct(id) > stock) {
+          const availableToAdd = stock - unitsPerProduct(id);
+          return alert(`Solamente podes agregar ${availableToAdd} productos`);
+        }
+        setCountToAdd(count);
+        addToCart(product, count);
+      };
+    
     return (
         <div>
-            <img src={item.image} alt="producto" />
-            <h2> {item.tittle}</h2>
-            <h4>{item.price}</h4>
+            <img src={image} alt="producto" />
+            <h2> {tittle}</h2>
+            <h4>{price}</h4>
             <div className='itemCounterContainer'>
-                <button> <Link to='/Cart'/>Finalizar Compra </button>
-                <ItemCount initial={0} stock={item.stock} onAdd={onAddCart} />
+                {countToAdd === 0 ? (
+                    <ItemCount initial={0} stock={stock}  onAdd={handleOnAdd} />
+                ) : (
+                    <Cart />
+                )}
             </div>
         </div>
-    )
-}
-
-
+    );
+};
 
 
 
